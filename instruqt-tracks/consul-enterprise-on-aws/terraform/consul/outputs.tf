adcc5cb85bb3df0a70291949f2e24aafca59ed19
Lance Larsen <lance@hashicorp.com>
2020-03-28 17:28:17 -0500

    working updates

--- instruqt-tracks/consul-enterprise-on-aws/terraform/consul/outputs.tf
+++ instruqt-tracks/consul-enterprise-on-aws/terraform/consul/outputs.tf
@@ -5,3 +5,18 @@ output "env" {
 output "lb" {
   value = aws_lb.consul.dns_name
 }
+
+output "master_token" {
+  value = module.consul.master_token
+  sensitive   = true
+}
+
+output "agent_server_token" {
+  value =  module.consul.agent_server_token
+  sensitive   = true
+}
+
+output "gossip_key" {
+  value = module.consul.gossip_key
+  sensitive   = true
+}
