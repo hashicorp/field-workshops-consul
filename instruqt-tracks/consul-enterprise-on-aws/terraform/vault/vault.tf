7fa863f5b25e33e21a57f14af4ec4469c0764236
Lance Larsen <lance@hashicorp.com>
2020-03-31 10:17:17 -0500

    working updates

--- instruqt-tracks/consul-enterprise-on-aws/terraform/vault/vault.tf
+++ instruqt-tracks/consul-enterprise-on-aws/terraform/vault/vault.tf
@@ -42,7 +42,7 @@ resource "aws_security_group" "vault" {
 }
 
 resource "aws_instance" "vault" {
-  instance_type               = "t2.small"
+  instance_type               = "t3.small"
   ami                         = "${data.aws_ami.ubuntu.id}"
   key_name                    = "instruqt"
   vpc_security_group_ids      = ["${aws_security_group.vault.id}"]
