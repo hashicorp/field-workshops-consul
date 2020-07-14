2130850c0edd5d702c5522886e54c18aa40d524e
Lance Larsen <lance@hashicorp.com>
2020-07-14 16:45:31 -0500

    working updates

--- instruqt-tracks/f5-on-azure-app-modernization-with-terraform-consul/assets/terraform/aks/main.tf
+++ instruqt-tracks/f5-on-azure-app-modernization-with-terraform-consul/assets/terraform/aks/main.tf
@@ -19,7 +19,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   default_node_pool {
     name           = "default"
-    node_count     = 1
+    node_count     = 2
     vm_size        = "Standard_D2_v2"
     vnet_subnet_id = data.terraform_remote_state.vnet.outputs.aks_subnets[0]
   }
