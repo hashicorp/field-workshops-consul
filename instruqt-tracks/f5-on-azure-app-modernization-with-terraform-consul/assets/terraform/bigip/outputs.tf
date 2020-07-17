f31c2246a60dde4076d4c15628f216ac25b83853
Kevin Corbin <kcorbin@hashicorp.com>
2020-07-17 15:54:53 -0500

    working updates

--- instruqt-tracks/f5-on-azure-app-modernization-with-terraform-consul/assets/terraform/bigip/outputs.tf
+++ instruqt-tracks/f5-on-azure-app-modernization-with-terraform-consul/assets/terraform/bigip/outputs.tf
@@ -21,3 +21,7 @@ output "admin_password" {
 output "vip_internal_address" {
   value = azurerm_network_interface.ext-nic.private_ip_address
 }
+
+output "ambassador_url" {
+  value = "https://${azurerm_public_ip.sip_public_ip.ip_address}"
+}
