6a38ca6c49e2b1e09f94c2e847c93477be967b89
Lance Larsen <lance@hashicorp.com>
2020-11-06 21:08:45 -0500

    working updates

--- instruqt-tracks/multi-cloud-service-networking-with-consul/assets/terraform/k8s-scheduler-services/main.tf
+++ instruqt-tracks/multi-cloud-service-networking-with-consul/assets/terraform/k8s-scheduler-services/main.tf
@@ -1,30 +1,9 @@
 data "google_client_config" "provider" {}
 
-provider "aws" {
-  version = "~> 3.0"
-  region  = "us-east-1"
-}
-
 provider "google" {
   version = "~> 3.3.0"
   region  = "us-central1"
   project = var.gcp_project_id
-}
-
-data "terraform_remote_state" "infra" {
-  backend = "local"
-
-  config = {
-    path = "../infra/terraform.tfstate"
-  }
-}
-
-data "terraform_remote_state" "iam" {
-  backend = "local"
-
-  config = {
-    path = "../iam/terraform.tfstate"
-  }
 }
 
 provider "kubernetes" {
--- instruqt-tracks/multi-cloud-service-networking-with-consul/assets/terraform/k8s-scheduler-services/outputs.tf
+++ instruqt-tracks/multi-cloud-service-networking-with-consul/assets/terraform/k8s-scheduler-services/outputs.tf
@@ -1,7 +1,3 @@
-output "aws_nomad_server_public_ip" {
-  value = aws_instance.nomad.public_ip
-}
-
 output "gcp_gke_cluster_react_name" {
   value = google_container_cluster.react.name
 }
