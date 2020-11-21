79f0552ed81b14cbb523da26b14cc189b22a4e4a
Lance Larsen <lance@hashicorp.com>
2020-11-21 09:08:52 -0500

    working updates

--- instruqt-tracks/multi-cloud-service-networking-with-consul/assets/terraform/k8s-scheduler-services/gke.tf
+++ instruqt-tracks/multi-cloud-service-networking-with-consul/assets/terraform/k8s-scheduler-services/gke.tf
@@ -3,7 +3,7 @@ resource "google_container_cluster" "graphql" {
   project            = var.gcp_project_id
   name               = "graphql-${data.terraform_remote_state.infra.outputs.env}"
   location           = "us-central1-a"
-  initial_node_count = 2
+  initial_node_count = 1
 
   network = "vpc-shared-svcs"
   subnetwork = "shared"
@@ -45,7 +45,7 @@ resource "google_container_cluster" "react" {
   project            = var.gcp_project_id
   name               = "react-${data.terraform_remote_state.infra.outputs.env}"
   location           = "us-central1-a"
-  initial_node_count = 2
+  initial_node_count = 1
 
   network = "vpc-shared-svcs"
   subnetwork = "shared"
