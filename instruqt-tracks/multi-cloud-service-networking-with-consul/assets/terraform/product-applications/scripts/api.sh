e8f34047b97a14b5645c72a9391fa3ef50850317
lanceplarsen <lance@hashicorp.com>
2021-04-27 13:23:01 -0500

    fix product api & db images

--- instruqt-tracks/multi-cloud-service-networking-with-consul/assets/terraform/product-applications/scripts/api.sh
+++ instruqt-tracks/multi-cloud-service-networking-with-consul/assets/terraform/product-applications/scripts/api.sh
@@ -170,7 +170,7 @@ sudo systemctl start envoy.service
 sleep 5
 
 #install the application
-wget https://github.com/hashicorp-demoapp/product-api-go/releases/download/v0.0.13/product-api -O /product-api
+wget https://github.com/hashicorp-demoapp/product-api-go/releases/download/v0.0.15/product-api -O /product-api
 chmod +x /product-api
 cat <<EOF > /conf.json
 {
