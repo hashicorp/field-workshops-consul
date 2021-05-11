4ff0caef9fdb738c8851c1c0dee7fcecb888d649
lanceplarsen <lance@hashicorp.com>
2021-05-11 10:32:58 -0500

    packer bin checks

--- instruqt-tracks/multi-cloud-service-networking-with-consul/assets/packer/scripts/azure.sh
+++ instruqt-tracks/multi-cloud-service-networking-with-consul/assets/packer/scripts/azure.sh
@@ -21,4 +21,55 @@ curl -sL 'https://getenvoy.io/gpg' | sudo apt-key add -
 sudo apt-add-repository "deb [arch=amd64] https://dl.bintray.com/tetrate/getenvoy-deb $(lsb_release -cs) stable"
 sudo apt update -y && sudo apt install -y getenvoy-envoy=1.16.2.p0.ge98e41a-1p71.gbe6132a
 
+#pgk checks
+#azure cli
+az --version
+if [ $? -ne 0 ]
+then
+  echo "Error checking Azure cli version"
+  exit 1
+fi
+#consul
+consul --version
+if [ $? -ne 0 ]
+then
+  echo "Error checking Consul version"
+  exit 1
+fi
+#vault
+vault --version
+if [ $? -ne 0 ]
+then
+  echo "Error checking Vault version"
+  exit 1
+fi
+#nomad
+nomad --version
+if [ $? -ne 0 ]
+then
+  echo "Error checking Nomad version"
+  exit 1
+fi
+#docker
+docker --version
+if [ $? -ne 0 ]
+then
+  echo "Error checking Docker version"
+  exit 1
+fi
+#jq
+jq --version
+if [ $? -ne 0 ]
+then
+  echo "Error checking jq version"
+  exit 1
+fi
+#envoy
+envoy --version
+if [ $? -ne 0 ]
+then
+  echo "Error checking Envoy version"
+  exit 1
+fi
+
 exit 0
