582a7f514ce2871ee16d78ea11febed06b4ba9f7
Nathan Pearce <nathan.pearce@gmail.com>
2022-01-10 14:55:53 -0800

    all of the outputs

--- instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-hcp-consul/outputs.tf
+++ instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-hcp-consul/outputs.tf
@@ -1,8 +1,20 @@
+output "aws_vpc_id" {
+  value = "${aws_vpc.vpc_services.id}"
+}
 output "hcp_consul_public_endpoint_url" {
   value = "${hcp_consul_cluster.workshop_hcp_consul.consul_public_endpoint_url}"
 }
-output "hcp_consul_config_file" {
+output "hcp_consul_private_endpoint_url" {
+  value = "${hcp_consul_cluster.workshop_hcp_consul.consul_public_endpoint_url}"
+}
+output "hcp_consul_config_file_decoded" {
   value = "${base64decode(hcp_consul_cluster.workshop_hcp_consul.consul_config_file)}"
+}
+output "hcp_consul_config_file" {
+  value = "${hcp_consul_cluster.workshop_hcp_consul.consul_config_file}"
+}
+output "consul_datacenter" {
+  value = "${hcp_consul_cluster.workshop_hcp_consul.datacenter}"
 }
 output "hcp_consul_version" {
   value = "${hcp_consul_cluster.workshop_hcp_consul.consul_version}"
