1ff3c9c7db60a99f2fb57fda4fb48aee4e918b5f
Nathan Pearce <nathan.pearce@gmail.com>
2022-01-20 08:25:51 -0800

    pull region from HCP output

--- instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-ecs-services/modules.tf
+++ instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-ecs-services/modules.tf
@@ -13,7 +13,7 @@ module "acl_controller" {
   consul_bootstrap_token_secret_arn = aws_secretsmanager_secret.bootstrap_token.arn
   consul_server_http_addr           = data.terraform_remote_state.hcp.outputs.hcp_consul_public_endpoint_url
   ecs_cluster_arn                   = aws_ecs_cluster.this.arn
-  region                            = var.region
+  region                            = var.vpc_region
   subnets                           = var.private_subnets_ids
   name_prefix                       = var.name
 }
