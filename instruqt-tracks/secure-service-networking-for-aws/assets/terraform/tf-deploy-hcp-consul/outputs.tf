392c83f5030e27bbd62624a596a3a35ccebb3cdb
Nathan Pearce <nathan.pearce@gmail.com>
2022-02-06 19:05:31 -0800

    terraform upgrade fixes

--- instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-hcp-consul/outputs.tf
+++ instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-hcp-consul/outputs.tf
@@ -17,26 +17,28 @@ output "hcp_consul_private_endpoint_url" {
   value = "${hcp_consul_cluster.workshop_hcp_consul.consul_private_endpoint_url}"
 }
 output "hcp_consul_config_file_decoded" {
-  value = "${base64decode(hcp_consul_cluster.workshop_hcp_consul.consul_config_file)}"
+  value = base64decode(hcp_consul_cluster.workshop_hcp_consul.consul_config_file)
 }
 output "hcp_consul_config_file" {
-  value = "${hcp_consul_cluster.workshop_hcp_consul.consul_config_file}"
+  value = hcp_consul_cluster.workshop_hcp_consul.consul_config_file
 }
 output "consul_datacenter" {
-  value = "${hcp_consul_cluster.workshop_hcp_consul.datacenter}"
+  value = hcp_consul_cluster.workshop_hcp_consul.datacenter
 }
 output "hcp_consul_version" {
-  value = "${hcp_consul_cluster.workshop_hcp_consul.consul_version}"
+  value = hcp_consul_cluster.workshop_hcp_consul.consul_version
 }
 output "hcp_consul_ca_file" {
-  value = "${hcp_consul_cluster.workshop_hcp_consul.consul_ca_file}"
+  value = hcp_consul_cluster.workshop_hcp_consul.consul_ca_file
 }
 output "hcp_acl_token" {
-  value = "${hcp_consul_cluster_root_token.token}"
+  value = hcp_consul_cluster_root_token.token
+  sensitive = true
 }
 output "hcp_acl_token_secret_id" {
-  value = "${hcp_consul_cluster_root_token.token.secret_id}"
+  value = hcp_consul_cluster_root_token.token.secret_id
+  sensitive = true
 }
 output "vpc_region" {
-  value = "${data.aws_region.current}"
+  value = data.aws_region.current
 }
