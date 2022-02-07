293772293e9df0877e4bdac743d5b894349e3c71
Nathan Pearce <nathan.pearce@gmail.com>
2022-02-06 19:52:08 -0800

    remove demo app

--- instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-ecs-services/logging.tf
+++ instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-ecs-services/logging.tf
@@ -12,12 +12,12 @@ locals {
     }
   }
 
-  example_client_app_log_config = {
-    logDriver = "awslogs"
-    options = {
-      awslogs-group         = aws_cloudwatch_log_group.log_group.name
-      awslogs-region        = var.vpc_region
-      awslogs-stream-prefix = "client"
-    }
-  }
+#  example_client_app_log_config = {
+#    logDriver = "awslogs"
+#    options = {
+#      awslogs-group         = aws_cloudwatch_log_group.log_group.name
+#      awslogs-region        = var.vpc_region
+#      awslogs-stream-prefix = "client"
+#    }
+#  }
 }
--- instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-ecs-services/providers.tf
+++ instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-ecs-services/providers.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">3.0.0"
     }
+#    consul = {
+#      source = "hashicorp/consul"
+#      version = "2.14.0"
+#    }
   }
 }
 
