fbd35fc7917f67e837171fc725661ce61364b24f
Nathan Pearce <nathan.pearce@gmail.com>
2022-02-10 06:55:02 -0800

    subnets only support in us-west-2n az's

--- instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-hcp-consul/vpc.tf
+++ instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-hcp-consul/vpc.tf
@@ -6,8 +6,8 @@ module "vpc_services_eks_dev" {
   name = "vpc_services_eks_dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-#  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+#  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
@@ -28,8 +28,8 @@ module "vpc_services_eks_prod" {
 #  name = "vpc_services_ecs"
   cidr = "10.1.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-#  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+#  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
   private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
   public_subnets  = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
 
