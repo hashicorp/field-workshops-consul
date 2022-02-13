a157bc3f71758b949d2d9b9d9498510fd3e6f46e
Nathan Pearce <nathan.pearce@gmail.com>
2022-02-13 00:06:05 -0800

    Fingers crossed

---
+++ instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-eks-services-prod/modules/hcp-eks-client/providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.4.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.3.0"
+    }
+  }
+}
