a0a8dc78c2a0e09b54724951d92e3e3a6f5d2ce3
Nathan Pearce <nathan.pearce@gmail.com>
2022-02-13 08:22:54 -0800

    workaround HCP az limit

--- instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-hcp-consul/vpc.tf
+++ instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-hcp-consul/vpc.tf
@@ -10,9 +10,10 @@ module "vpc_eks_dev" {
   name = "vpc_eks_dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+#  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+#  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+#  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
@@ -29,9 +30,10 @@ module "vpc_eks_prod" {
   name = "vpc_eks_prod"
   cidr = "10.1.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
-  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
+#  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+#  private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+#  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
+  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true
