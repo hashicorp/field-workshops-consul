83038139c758c35847191c73e558ce7f2ba66fe2
Hariram Sankaran <56744845+ramramhariram@users.noreply.github.com>
2022-02-12 21:46:08 -0800

    Update outputs.tf

--- instruqt-tracks/multi-cloud-service-networking-with-consul/assets/terraform/esm/outputs.tf
+++ instruqt-tracks/multi-cloud-service-networking-with-consul/assets/terraform/esm/outputs.tf
@@ -6,6 +6,6 @@ output "azure_esm_public_ip" {
   value = azurerm_public_ip.esm.ip_address
 }
 
-output "aws_esm_private_ip " {
+output "aws_esm_private_ip" {
   value = aws_instance.esm.private_ip 
 }
