37160736b558fc67a72a52eda824688aa57adf6c
Nathan Pearce <nathan.pearce@gmail.com>
2022-02-13 11:15:49 -0800

    reduce subnets for HCN

--- instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-hcp-consul/vpc.tf
+++ instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-hcp-consul/vpc.tf
@@ -10,8 +10,9 @@ module "vpc_eks_dev" {
   name = "vpc_eks_dev"
   cidr = "10.0.0.0/16"
 
-#  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["us-west-2a", "us-west-2b"]
 #  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
 #  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
@@ -30,8 +31,9 @@ module "vpc_eks_prod" {
   name = "vpc_eks_prod"
   cidr = "10.1.0.0/16"
 
-#  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  azs             = ["us-west-2a", "us-west-2b"]
 #  private_subnets = ["10.1.1.0/24", "10.1.2.0/24", "10.1.3.0/24"]
+  private_subnets = ["10.1.1.0/24", "10.1.2.0/24"]
 #  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24", "10.1.103.0/24"]
   public_subnets  = ["10.1.101.0/24", "10.1.102.0/24"]
 
