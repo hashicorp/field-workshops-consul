5b08a24bd92edc1c193b11f914bb16130a0a2c24
Nathan Pearce <nathan.pearce@gmail.com>
2022-03-31 10:53:54 -0700

    revert

--- instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-eks-services-dev/main.tf
+++ instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-eks-services-dev/main.tf
@@ -154,9 +154,9 @@ output "consul_url" {
   )
 }
 
-#output "kubeconfig_filename" {
-#  value = abspath(module.eks.kubeconfig_filename)
-#}
+output "kubeconfig_filename" {
+  value = abspath(module.eks.kubeconfig_filename)
+}
 
 output "hashicups_url" {
   value = module.demo_app.hashicups_url
