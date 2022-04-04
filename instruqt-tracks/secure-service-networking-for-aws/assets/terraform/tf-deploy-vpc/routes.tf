3a57af5e2a5d3600d6d58acd6ca3968f85fbf1bf
Nathan Pearce <nathan.pearce@gmail.com>
2022-04-04 14:00:58 -0700

    HCP HVN to/from VPC Routes

--- instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-vpc/routes.tf
+++ instruqt-tracks/secure-service-networking-for-aws/assets/terraform/tf-deploy-vpc/routes.tf
@@ -1,25 +1,24 @@
-
-#module "aws_hcp_consul" {
-#  source  = "hashicorp/hcp-consul/aws"
-#  version = "~> 0.6.1"
-#
-#  hvn                = local.hvn
-#  vpc_id             = local.vpc_id
-#  subnet_ids         = local.public_subnets
-#  route_table_ids    = local.public_route_table_ids
-#  security_group_ids = [module.eks.cluster_primary_security_group_id]
-#}
 locals {
-  hvn                   = data.terraform_remote_state.hcp.outputs.hcp_hvn
-  ecs_dev_vpc_id        = module.vpc_ecs_dev.vpc_id
-  ecs_dev_vpc_owner_id  = module.vpc_ecs_dev.vpc_owner_id
+  hvn                       = data.terraform_remote_state.hcp.outputs.hcp_hvn
+  ecs_dev_vpc_id            = module.vpc_ecs_dev.vpc_id
+  ecs_dev_vpc_owner_id      = module.vpc_ecs_dev.vpc_owner_id
+  ecs_dev_route_table_ids   = concat(module.vpc_ecs_dev.private_route_table_ids, module.vpc_ecs_dev.public_route_table_ids)
+  eks_dev_vpc_id            = module.vpc_eks_dev.vpc_id
+  eks_dev_vpc_owner_id      = module.vpc_eks_dev.vpc_owner_id
+  eks_dev_route_table_ids   = concat(module.vpc_eks_dev.private_route_table_ids, module.vpc_eks_dev.public_route_table_ids)
+  eks_prod_vpc_id           = module.vpc_eks_prod.vpc_id
+  eks_prod_vpc_owner_id     = module.vpc_eks_prod.vpc_owner_id
+  eks_prod_route_table_ids  = concat(module.vpc_eks_prod.private_route_table_ids, module.vpc_eks_prod.public_route_table_ids)
 }
 
 
-### Peering between HCP HVNs and VPCs
 
-resource "aws_vpc_peering_connection_accepter" "hvn" {
-  vpc_peering_connection_id = hcp_aws_network_peering.peer.provider_peering_id
+## Peering between HCP HVNs and VPCs
+
+### HCP HVN to ECS Dev
+
+resource "aws_vpc_peering_connection_accepter" "hvn_to_ecs_dev" {
+  vpc_peering_connection_id = hcp_aws_network_peering.ecs_dev_peer.provider_peering_id
   auto_accept               = true
 }
 
@@ -31,12 +30,26 @@ resource "hcp_aws_network_peering" "ecs_dev_peer" {
   peering_id      = local.hvn.hvn_id
 }
 
+### HCP HVN to EKS Dev
+
+resource "aws_vpc_peering_connection_accepter" "hvn_to_eks_dev" {
+  vpc_peering_connection_id = hcp_aws_network_peering.eks_dev_peer.provider_peering_id
+  auto_accept               = true
+}
+
 resource "hcp_aws_network_peering" "eks_dev_peer" {
   hvn_id          = local.hvn.hvn_id
   peer_vpc_id     = local.eks_dev_vpc_id
   peer_account_id = local.eks_dev_vpc_owner_id
   peer_vpc_region = var.region
   peering_id      = local.hvn.hvn_id
+}
+
+### HCP HVN to EKS Prod
+
+resource "aws_vpc_peering_connection_accepter" "hvn_to_eks_prod" {
+  vpc_peering_connection_id = hcp_aws_network_peering.eks_prod_peer.provider_peering_id
+  auto_accept               = true
 }
 
 resource "hcp_aws_network_peering" "eks_prod_peer" {
@@ -48,38 +61,50 @@ resource "hcp_aws_network_peering" "eks_prod_peer" {
 }
 
 
-### Routes from VPCs to HCP HVN
 
-locals {
-  route_table_ids = concat(module.vpc_ecs_dev.default_route_table_id, module.vpc_eks_dev.default_route_table_id, module.vpc_eks_prod.default_route_table_id)
-}
-resource "aws_route" "hvn" {
-  count                     = length(local.route_table_ids)
-  route_table_id            = local.route_table_ids[count.index]
-  destination_cidr_block    = local.hvn.cidr_block
-  vpc_peering_connection_id = hcp_aws_network_peering.peer.provider_peering_id
-}
-
-
-### Routes from HCP HVN to VPCs
+## Routes from HCP HVN to VPCs
 
 resource "hcp_hvn_route" "hvn_to_vpc_ecs_dev" {
-  hvn_link         = hcp_hvn.hvn.self_link
-  hvn_route_id     = "${hcp_hvn.hvn.hvn_id}-to-vpc_ecs_dev"
+  hvn_link         = local.hvn.self_link
+  hvn_route_id     = "${local.hvn.hvn_id}-to-vpc-ecs-dev"
   destination_cidr = module.vpc_ecs_dev.vpc_cidr_block
-  target_link      = hcp_aws_network_peering.peer.self_link
+  target_link      = hcp_aws_network_peering.ecs_dev_peer.self_link
 }
 
 resource "hcp_hvn_route" "hvn_to_vpc_eks_dev" {
-  hvn_link         = hcp_hvn.hvn.self_link
-  hvn_route_id     = "${hcp_hvn.hvn.hvn_id}-to-vpc_eks_dev"
+  hvn_link         = local.hvn.self_link
+  hvn_route_id     = "${local.hvn.hvn_id}-to-vpc-eks-dev"
   destination_cidr = module.vpc_eks_dev.vpc_cidr_block
-  target_link      = hcp_aws_network_peering.peer.self_link
+  target_link      = hcp_aws_network_peering.eks_dev_peer.self_link
 }
 
 resource "hcp_hvn_route" "hvn_to_vpc_eks_prod" {
-  hvn_link         = hcp_hvn.hvn.self_link
-  hvn_route_id     = "${hcp_hvn.hvn.hvn_id}-to-vpc_eks_prod"
+  hvn_link         = local.hvn.self_link
+  hvn_route_id     = "${local.hvn.hvn_id}-to-vpc-eks-prod"
   destination_cidr = module.vpc_eks_prod.vpc_cidr_block
-  target_link      = hcp_aws_network_peering.peer.self_link
+  target_link      = hcp_aws_network_peering.eks_prod_peer.self_link
+}
+
+
+
+## Routes from VPCs to HCP HVN
+resource "aws_route" "hvn_to_ecs_dev" {
+  count                     = length(local.ecs_dev_route_table_ids)
+  route_table_id            = local.ecs_dev_route_table_ids[count.index]
+  destination_cidr_block    = local.hvn.cidr_block
+  vpc_peering_connection_id = hcp_aws_network_peering.ecs_dev_peer.provider_peering_id
+}
+
+resource "aws_route" "hvn_to_eks_dev" {
+  count                     = length(local.eks_dev_route_table_ids)
+  route_table_id            = local.eks_dev_route_table_ids[count.index]
+  destination_cidr_block    = local.hvn.cidr_block
+  vpc_peering_connection_id = hcp_aws_network_peering.eks_dev_peer.provider_peering_id
+}
+
+resource "aws_route" "hvn_to_eks_prod" {
+  count                     = length(local.eks_prod_route_table_ids)
+  route_table_id            = local.eks_prod_route_table_ids[count.index]
+  destination_cidr_block    = local.hvn.cidr_block
+  vpc_peering_connection_id = hcp_aws_network_peering.eks_prod_peer.provider_peering_id
 }
