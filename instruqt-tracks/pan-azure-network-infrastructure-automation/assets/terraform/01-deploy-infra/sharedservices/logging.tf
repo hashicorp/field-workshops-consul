0066c341f224de02c98e94e965793663dc58f007
David Cañadillas <41782403+dcanadillas@users.noreply.github.com>
2023-11-10 15:58:58 +0100

    Update Consul version

--- instruqt-tracks/pan-azure-network-infrastructure-automation/assets/terraform/01-deploy-infra/sharedservices/logging.tf
+++ instruqt-tracks/pan-azure-network-infrastructure-automation/assets/terraform/01-deploy-infra/sharedservices/logging.tf
@@ -88,7 +88,7 @@ resource "azurerm_linux_virtual_machine" "logging" {
   }
   custom_data = base64encode(templatefile("${path.module}/scripts/logging.sh", { 
     consul_server_ip = azurerm_network_interface.consul.private_ip_address,
-    CONSUL_VERSION = "1.12.2" 
+    CONSUL_VERSION = "1.16.3" 
   }))
 
   computer_name                   = "logging-vm"
