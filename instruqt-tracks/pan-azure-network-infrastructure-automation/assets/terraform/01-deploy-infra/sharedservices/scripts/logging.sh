7eaae7cc2e6733945b12f694a534f19876790151
dcanadillas <dcanadillas@hashicorp.com>
2023-11-10 16:51:14 +0100

    Updating Docker for VMs

--- instruqt-tracks/pan-azure-network-infrastructure-automation/assets/terraform/01-deploy-infra/sharedservices/scripts/logging.sh
+++ instruqt-tracks/pan-azure-network-infrastructure-automation/assets/terraform/01-deploy-infra/sharedservices/scripts/logging.sh
@@ -10,11 +10,28 @@ sudo apt-get update
 sudo apt-get install -y jq
 
 
-
 #Install Dockers
-sudo snap install docker
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo apt-get install -y ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker
+
+sudo apt install -y docker-compose
+
+# # 
+# sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
 
 
 #Download Consul
